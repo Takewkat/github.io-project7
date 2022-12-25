72658dd32bbe11fe760f00cdf987fb1ea5f0ca48
Iana Kaminska <65045059+Takewkat@users.noreply.github.com>
2022-12-25 15:57:26 +0100

    add hashrouter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+//HashRouter is used instead of BrowserRouter because of the github pages deployment
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App /> 
-  </BrowserRouter>
+  </HashRouter>
 );
